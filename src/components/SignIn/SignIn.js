acc1f57d51e826cc50f5c2571da8404f9efaedc7
Beattris <beatriceebirim@gmail.com>
2022-01-26 17:52:27 +0100

    finished the UI

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -4,15 +4,15 @@ const SignIn = (props) => {
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-1 mw6 shadow-5 center">
         <main className="pa4 black-80">
-         <form className="measure">
+         <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f4 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
-                <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                 <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
             </div>
             <div className="mv3">
-                <label className="db fw6 lh-copy f6" for="password">Password</label>
+                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                 <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
             </div>
             </fieldset>
@@ -22,7 +22,7 @@ const SignIn = (props) => {
             <div className="lh-copy mt3">
             <p className="f6 link dim black db pointer" onClick={() => props.onRouteChange('register')}>Register</p>
             </div>
-         </form> 
+         </div> 
         </main>
     </article>
   );
