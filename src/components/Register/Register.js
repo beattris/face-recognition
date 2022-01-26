d7d6c75b3154ea355d66deed10c711925aafbdda
Beattris <beatriceebirim@gmail.com>
2022-01-26 17:54:18 +0100

    finished the UI

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -6,7 +6,7 @@ const Register = (props) => {
         <main className="pa4 black-80">
          <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f4 fw6 ph0 mh0">Register</legend>
+              <legend className="f2 fw6 ph0 mh0">Register</legend>
               <div className="mt3">
                   <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
                   <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -6,7 +6,7 @@ const SignIn = (props) => {
         <main className="pa4 black-80">
          <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+            <legend className="f2 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                 <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
