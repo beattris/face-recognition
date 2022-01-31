f173be1f05174198f6dc3750abbe3394943010e0
Beattris <beatriceebirim@gmail.com>
2022-01-31 04:06:29 +0100

    updated the signin component

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -27,8 +27,9 @@ class SignIn extends Component {
      })
     })
       .then(response => response.json())
-      .then(data => {
-        if (data === 'success'){
+      .then(user => {
+        if (user.id){
+          this.props.loadUser(user);
           this.props.onRouteChange('home');
         }
       })
