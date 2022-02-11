c4a1e1320cca3d25421f20054f3047ef4fdd6eb3
Beattris <beatriceebirim@gmail.com>
2022-02-11 02:35:32 +0100

    successfully connected the database, project completed

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -34,7 +34,7 @@ class Register extends Component {
     })
       .then(response => response.json())
       .then(user => {
-        if (user){
+        if (user.id){
           this.props.loadUser(user);
           this.props.onRouteChange('home');
         }
